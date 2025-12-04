ce085eb2d9ca6227550e8729107bf485b9e5eba1
卜部 昌平 <s-urabe@sakura.ad.jp>
2025-12-04 11:36:17 +0900

    improve error message
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

--- apis/folder/folder.go
+++ apis/folder/folder.go
@@ -61,7 +61,7 @@ func (f *folderOp) Create(ctx context.Context, params CreateParams) (*v1.Folder,
 }
 
 func (f *folderOp) Read(ctx context.Context, id int) (*v1.Folder, error) {
-	return iam.ErrorFromDecodedResponse[v1.Folder]("Folder.Get", func() (any, error) {
+	return iam.ErrorFromDecodedResponse[v1.Folder]("Folder.Read", func() (any, error) {
 		return f.client.FoldersFolderIDGet(ctx, v1.FoldersFolderIDGetParams{FolderID: id})
 	})
 }
